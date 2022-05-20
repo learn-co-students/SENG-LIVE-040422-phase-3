90641016861a3f4100fa6ba2a5aa9b1d9159992b
Dakota L Martinez <dakotaleemusic@gmail.com>
2022-05-20 10:46:38 -0700

    fix environment file

--- 05_intro_to_activerecord/demo/config/environment.rb
+++ 05_intro_to_activerecord/demo/config/environment.rb
@@ -1,7 +1,9 @@
+ENV["AR_ENVIRONMENT"] ||= "development"
 # require is like an import statement except it loads all methods from the required file (not just the default export)
 require "bundler/setup"
 # Here we're calling a method that requires all of the ruby gems in the default environment (not in a group like development or test) For our purposes, this will load require_all and pry so that we can use `require_all` and `binding.pry` within our code
-Bundler.require(:default)
+Bundler.require(:default, ENV["AR_ENVIRONMENT"])
+
 require "date"
 require "active_support/core_ext/integer/time"
 require "active_support/core_ext/date/calculations"
@@ -9,4 +11,9 @@ require "active_support/core_ext/date/calculations"
 
 DB = SQLite3::Database.new("db/dog_walker.db")
 DB.results_as_hash = true
+
+# our application won't support different timezones 
+# we'll default to displaying all times in local time
+# this will get around the problem with the offset for now
+ActiveRecord::Base.default_timezone = :local
 require_all "lib"
