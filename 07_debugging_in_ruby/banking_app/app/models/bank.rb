ad347871bd78c6d1adb6f15033363cc29237b77a
Dakota L Martinez <dakotaleemusic@gmail.com>
2022-05-25 09:54:40 -0700

    complete solution

--- 07_debugging_in_ruby/banking_app/app/models/bank.rb
+++ 07_debugging_in_ruby/banking_app/app/models/bank.rb
@@ -2,7 +2,21 @@ class Bank < ActiveRecord::Base
   has_many :accounts
   has_many :users, -> { distinct }, through: :accounts
 
+  def open_account(user, label, account_type, opening_deposit)
+    Account.create(
+      user: user,
+      bank: self,
+      label: label,
+      balance: opening_deposit,
+      account_type: account_type
+    )
+  end
+
   def accounts_summary(user)
     self.accounts.where(user: user).map(&:summary)
+  end
+
+  def blacklist(user)
+    Account.where(user_id: user.id, bank_id: self.id).destroy_all
   end
 end
